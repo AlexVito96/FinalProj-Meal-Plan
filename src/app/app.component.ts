c091ab8c2aedc4473fcd73976f26b99d889dfb52
AlexVito96 <59684198+AlexVito96@users.noreply.github.com>
2020-06-30 17:59:13 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ERP';
+  myName = 'Alex Vito'
+
+  constructor() {
+    console.log('I am the constructor');
+
+    var name: string = 'Alex Vito';
+
+    this.hello(this.title);
+    this.hello(9);
+  }
+
+  hello(name) {
+    console.log(name);
+  }
+  
 }
